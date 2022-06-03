3c62cceb277d65f269f3ecf40b020ab8a0c467ac
dxps <marius.ileana@gmail.com>
2022-06-03 22:26:01 +0300

    feat(macros)

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,15 +1,15 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
-
 macro_rules! my_macro {
+    // first arm for no args
     () => {
         println!("Check out my macro!");
-    }
+    };
+    // second arm for one arg
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
